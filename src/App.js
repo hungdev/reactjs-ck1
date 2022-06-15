9905cc9b3466ac9de48c9068d66318aa5c6ccd27
hungvu <hungvu@fortna.com>
2022-06-15 20:22:34 +0700

    add redux perist

--- src/App.js
+++ src/App.js
@@ -1,23 +1,55 @@
 // import { createStore } from "redux";
+import { combineReducers } from "redux";
 import { configureStore } from '@reduxjs/toolkit';
 import { Provider } from "react-redux";
 import AppContainer from "./AppContainer";
 // import allReducer from "./reducers";
 import productSlice from "./reducers/productSlice";
 // import store from './store';
+// config redux perist
+import { PersistGate } from 'redux-persist/integration/react';
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 export default function App() {
-  // const store = createStore(allReducer);
-  const store = configureStore({
-    reducer: {
-      productSlice: productSlice,
-      // them cac reducer o day
-    }
+
+  const persistConfig = {
+    key: 'root',
+    storage: storage
+  };
+
+  const allReducers = combineReducers({
+    productSlice: productSlice,
   });
+
+  const persistedReducer = persistReducer(persistConfig, allReducers);
+
+  const store = configureStore({
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) =>
+      getDefaultMiddleware({
+        // serializableCheck: {
+        //   ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+        // },
+        serializableCheck: false,
+      }),
+  });
+  let persistor = persistStore(store);
 
   return (
     <Provider store={store}>
-      <AppContainer />
+      <PersistGate loading={null} persistor={persistor}>
+        <AppContainer />
+      </PersistGate>
     </Provider>
   );
 }
